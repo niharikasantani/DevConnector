0215eeefb4133a2aa74981ba7490dbb75b388a7a
Niharika <niharika.santani@gmail.com>
2020-06-28 20:55:30 +0530

    commit after initial react setup and register, load user, login tokens generation

---
+++ client/src/actions/types.js
@@ -0,0 +1,15 @@
+//alert
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
+
+//auth
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+
+// Load User and Set auth token
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+
+//login auth
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
