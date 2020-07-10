ce059f2093833315a5bf01491fe3bed9c6bca190
Niharika <niharika.santani@gmail.com>
2020-07-10 23:15:06 +0530

    application complete

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDb = require('./config/db');
 const app = express();
+const path = require('path');
 
 //connect db
 
@@ -10,14 +11,22 @@ connectDb();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API running'));
-
 //Define routes
 
 app.use('/api/users', require('./route/api/users'));
 app.use('/api/posts', require('./route/api/posts'));
 app.use('/api/auth', require('./route/api/auth'));
 app.use('/api/profile', require('./route/api/profile'));
+
+// Serve static assets in production
+
+if (process.env.NODE_ENV === 'production') {
+	//set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 
